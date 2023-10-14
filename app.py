477bbbf336fd148f81b8ab2206bbe7b1bace9e3d
Prajwal Bhonagde <bhongadepk_1@rknec.edu>
2023-10-14 15:51:51 +0530

    first commit to aws

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask,request,render_template,jsonify
+
+
+
+
+application = Flask(__name__)
+
+@application.route('/')
+def index():
+    
+    return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+    application.run(debug=True)
