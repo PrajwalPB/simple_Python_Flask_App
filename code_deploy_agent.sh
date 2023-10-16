1088a5ee0ce3bb282f48f73cdb1e7589aaa00d3e
PrajwalPB <prajwalbhongade85@gmail.com>
2023-10-16 09:51:13 +0530

    first commit

---
+++ code_deploy_agent.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update
+sudo apt install ruby-full
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+sudo service codedeploy-agent status
